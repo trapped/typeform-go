6165e1e8268028df658fe42fcc58a8b265fff426
Giorgio Pellero <giorgio.pellero@typeform.com>
2022-01-07 13:15:34 +0100

    feat: add ThemeListParams.IncludePremiumThemes

--- theme_list.go
+++ theme_list.go
@@ -1,8 +1,9 @@
 package typeform
 
 type ThemeListParams struct {
-	Page     int `url:"page,omitempty"`
-	PageSize int `url:"page_size,omitempty"`
+	Page                 int  `url:"page,omitempty"`
+	PageSize             int  `url:"page_size,omitempty"`
+	IncludePremiumThemes bool `url:"premium_themes,omitempty"`
 }
 
 type ThemeList struct {
